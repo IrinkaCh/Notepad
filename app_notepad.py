a1144a32c234c694b8422871b44226fe1f5906d8
IrinkaCh <123irina00@mail.ru>
2023-02-28 22:57:48 +0500

    app_notepad.py

---
+++ app_notepad.py
@@ -0,0 +1,16 @@
+from notepad_function import Note_pad
+
+
+def app_notepad():
+    note_Pad = Note_pad()
+    print("Выберите действие:\n\
+    1) Создать новую заметку\n\
+    2) Просмотреть список всех заметок\n\
+    3) Выбрать заметку по названию, для дальнейших действий")
+    select = input()
+    if select == "1":
+        note_Pad.add_note()
+    if select == "2":
+        note_Pad.list_note()
+    if select == "3":
+        note_Pad.select_note()
